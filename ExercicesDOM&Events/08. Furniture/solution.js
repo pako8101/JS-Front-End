0d4da1833cf94e974f007216585c7d6035718340
Plamen Kamenov <pako810129@gmail.com>
2023-11-15 09:22:14 +0200

    Add files via upload

--- ExercicesDOM&Events/08. Furniture/solution.js
+++ ExercicesDOM&Events/08. Furniture/solution.js
@@ -1,42 +1,102 @@
 function solve() {
-  const button = document.querySelector("#exercise button");
-  button.addEventListener("click", parseFurnitureInput);
+  const formatButton = document.querySelector("#exercise button");
+  formatButton.addEventListener("click", parseFurnitureInput);
+
+  const buyButton = document.querySelector("#exercise button:nth-of-type(2)");
+  buyButton.addEventListener("click", buySelectedFurniture);
+
+  Array.from(document.querySelectorAll('input[type="checkbox"]')).forEach(
+    (checkbox) => checkbox.removeAttribute("disabled")
+  );
+}
+
+function buySelectedFurniture() {
+  const checkboxes = Array.from(
+    document.querySelectorAll('input[type="checkbox"]:checked')
+  );
+  // Hardest thing in the whole task
+  const cart = checkboxes.map(mapCheckboxToObject).reduce(
+    (acc, curr) => {
+      acc.names.push(curr.name);
+      acc.price += curr.price;
+      acc.averageDecorationFactor += curr.decFactor / checkboxes.length;
+
+      return acc;
+    },
+    {
+      names: [],
+      price: 0,
+      averageDecorationFactor: 0,
+    }
+  );
+
+  const cartTextArea = document.querySelector(
+    "#exercise textarea:nth-of-type(2)"
+  );
+  cartTextArea.value = `
+    Bought furniture: ${cart.names.join(", ")}
+    Total price: ${cart.price.toFixed(2)}
+    Avg Dec Factor: ${cart.averageDecorationFactor.toFixed(2)}
+  `;
+}
+
+function mapCheckboxToObject(checkbox) {
+  const row = checkbox.parentElement.parentElement;
+  const name = row.querySelector("td:nth-of-type(2)").innerText;
+  const price = Number(row.querySelector("td:nth-of-type(3)").innerText);
+  const decFactor = Number(row.querySelector("td:nth-of-type(4)").innerText);
+
+  return { name, price, decFactor };
 }
 
 function parseFurnitureInput() {
   const input = JSON.parse(document.querySelector("#exercise textarea").value);
   const tableBody = document.querySelector("tbody");
+  // TODO: naming
+  const cellCreator = createCellCreator();
+
   input
-    .map((furniture) => {
+    .map(cellCreator.createFurnitureRow)
+    .forEach((row) => tableBody.appendChild(row));
+}
+
+function createCellCreator() {
+  function createImageCell(src) {
+    const imageCell = document.createElement("td");
+    const image = document.createElement("img");
+    image.src = src;
+    imageCell.appendChild(image);
+
+    return imageCell;
+  }
+
+  function createTextCell(text) {
+    const cell = document.createElement("td");
+    cell.textContent = text;
+
+    return cell;
+  }
+
+  function createCheckboxCell() {
+    const checkCell = document.createElement("td");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkCell.appendChild(checkbox);
+
+    return checkCell;
+  }
+
+  return {
+    createFurnitureRow(furniture) {
       const row = document.createElement("tr");
 
-      const imageCell = document.createElement("td");
-      const image = document.createElement("img");
-      image.src = furniture.img;
-      imageCell.appendChild(image);
-
-      const nameCell = document.createElement("td");
-      nameCell.textContent = furniture.name;
-
-      const priceCell = document.createElement("td");
-      priceCell.textContent = furniture.price;
-
-      const factoryCell = document.createElement("td");
-      factoryCell.textContent = furniture.deFactor;
-
-      const checkCell = document.createElement("td");
-      const checkBox = document.createElement("input");
-      checkBox.type = "checkbox";
-      checkCell.appendChild(checkBox);
-      row.appendChild(imageCell);
-      row.appendChild(priceCell);
-      row.appendChild(nameCell);
-      row.appendChild(factoryCell);
-      row.appendChild(checkCell);
+      row.appendChild(createImageCell(furniture.img));
+      row.appendChild(createTextCell(furniture.name));
+      row.appendChild(createTextCell(furniture.price));
+      row.appendChild(createTextCell(furniture.decFactor));
+      row.appendChild(createCheckboxCell());
 
       return row;
-    })
-    .forEach((row) => {
-      tableBody.appendChild(row);
-    });
+    },
+  };
 }
