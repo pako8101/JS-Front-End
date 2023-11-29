b70a2c8b9521f384b022e9dd775ca551a39227d4
Plamen Kamenov <pako810129@gmail.com>
2023-11-29 08:42:31 +0200

    Add files via upload

--- SprintExamPrepJuly2023/02.Sprint-Planning/app.js
+++ SprintExamPrepJuly2023/02.Sprint-Planning/app.js
@@ -3,7 +3,6 @@ window.addEventListener("load", solve);
 function solve() {
   const tasks = {};
   const inputSelector = {
-    hiddenTaskId: document.querySelector("#task-id"),
     title: document.querySelector("#title"),
     description: document.querySelector("#description"),
     label: document.querySelector("#label"),
@@ -11,6 +10,7 @@ function solve() {
     assignee: document.querySelector("#assignee"),
   };
   const selectors = {
+    hiddenTaskId: document.querySelector("#task-id"),
     createButton: document.querySelector("#create-task-btn"),
     deleteButton: document.querySelector("#delete-task-btn"),
     tasksSection: document.querySelector("#tasks-section"),
@@ -70,7 +70,7 @@ function solve() {
     );
     createElement(
       "div",
-      `Assigned to ${task.assignee}`,
+      `Assigned to: ${task.assignee}`,
       ["task-card-assignee"],
       null,
       article
@@ -87,70 +87,71 @@ function solve() {
     const button = createElement("button", "Delete", [], null, taskActions);
     button.addEventListener("click", loadDeleteConfirm);
     selectors.tasksSection.appendChild(article);
+
     calcTotalPoints();
 
     Object.values(inputSelector).forEach((selector) => (selector.value = ""));
+  }
+  function calcTotalPoints() {
+    const totalPoints = Object.values(tasks).reduce(
+      (acc, curr) => acc + curr.points,
+      0
+    );
+    selectors.totalPoints.textContent = `Total points ${totalPoints}pts`;
+  }
 
-    function calcTotalPoints() {
-      const totalPoints = tasks.reduce((acc, curr) => {
-        acc + curr.points;
-      }, 0);
-      selectors.totalPoints.textContent = `Total points ${totalPoints}pts`;
-    }
-
-    function deleteTask() {
-      const taskId = selectors.hiddenTaskId.value;
-      const taskElement = document.querySelector(`#${taskId}`);
-      taskElement.remove();
-      delete tasks[taskId];
-      Object.values(inputSelector).forEach((selector) => {
-        selector.value = "";
-        selector.disabled = false;
-      });
-      selectors.createButton.disabled = false;
-      selectors.deleteButton.disabled = true;
-
-      calcTotalPoints();
-    }
-
-    function createElement(type, content, classes, id, parent, useInnerHTML) {
-      const element = document.createElement(type);
-      if (useInnerHTML && content) {
-        element.innerHTML = content;
-      } else if (content) {
-        element.textContent = content;
-      }
-      if (classes && classes.length > 0) {
-        element.classList.add(...classes);
-      }
-      if (id) {
-        element.setAttribute("id", id);
-      }
-      if (parent) {
-        parent.appendChild(element);
-      }
-      return element;
-    }
-    function loadDeleteConfirm(e) {
-      const taskId =
-        e.currentTarget.parentElement.parentElement.getAttribute("id");
-      Object.keys(inputSelector).forEach((key) => {
-        const selector = inputSelector.key;
-        selector.value = tasks[taskId][key];
-        selector.disabled = true;
-      });
-      /*for (const item of inputSelector.value) {
+  function loadDeleteConfirm(e) {
+    const taskId =
+      e.currentTarget.parentElement.parentElement.getAttribute("id");
+    Object.keys(inputSelector).forEach((key) => {
+      const selector = inputSelector[key];
+      selector.value = tasks[taskId][key];
+      selector.disabled = true;
+    });
+    /*for (const item of inputSelector.value) {
         item = tasks[taskId].value;
     }*/
-      /* inputSelector.title.value = tasks[taskId].title;
+    /* inputSelector.title.value = tasks[taskId].title;
       inputSelector.description.value = tasks[taskId].description;
       inputSelector.label.value = tasks[taskId].label;
       inputSelector.points.value = tasks[taskId].points;
       inputSelector.assignee.value = tasks[taskId].assignee;*/
 
-      inputSelector.hiddenTaskId.value = taskId;
-      selectors.createButton.disabled = true;
-      selectors.deleteButton.disabled = false;
+    selectors.hiddenTaskId.value = taskId;
+    selectors.createButton.disabled = true;
+    selectors.deleteButton.disabled = false;
+  }
+  function deleteTask() {
+    const taskId = selectors.hiddenTaskId.value;
+    const taskElement = document.querySelector(`#${taskId}`);
+    taskElement.remove();
+    delete tasks[taskId];
+    Object.values(inputSelector).forEach((selector) => {
+      selector.value = "";
+      selector.disabled = false;
+    });
+    selectors.createButton.disabled = false;
+    selectors.deleteButton.disabled = true;
+
+    calcTotalPoints();
+  }
+
+  function createElement(type, content, classes, id, parent, useInnerHTML) {
+    const element = document.createElement(type);
+    if (useInnerHTML && content) {
+      element.innerHTML = content;
+    } else if (content) {
+      element.textContent = content;
     }
+    if (classes && classes.length > 0) {
+      element.classList.add(...classes);
+    }
+    if (id) {
+      element.setAttribute("id", id);
+    }
+    if (parent) {
+      parent.appendChild(element);
+    }
+    return element;
   }
 }
